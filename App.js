caa5ae20b5e60f674410b3f34f1e5405e1c0e039
BernardoAbreu-git <bernardos.abreu12@gmail.com>
2025-09-22 08:54:33 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Image, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { styles } from './Styles';
 
 export default function App() {
@@ -11,87 +11,113 @@ export default function App() {
 
   const [controleNumeral, setControleNumeral] = useState(1);
 
-  function handleNumeral(numero){
-    if(controleNumeral===1){
-      setPrimeiroNumero(primeiroNumero  + numero)
-    }else{
-      setSegundoNumero(segundoNumero  + numero)
+  function handleNumeral(numero) {
+    if (controleNumeral === 1) {
+      setPrimeiroNumero(primeiroNumero + numero);
+    } else {
+      setSegundoNumero(segundoNumero + numero);
     }
   }
 
-  function handleOperacao(operacao){
-    setOperador(operacao)
-    setControleNumeral(2)
+  function handleOperacao(operacao) {
+    setOperador(operacao);
+    setControleNumeral(2);
   }
 
-  function handleClear(){
-    setPrimeiroNumero('')
-    setSegundoNumero('')
-    setOperador(null)
-    setControleNumeral(1)
+  function handleClear() {
+    setPrimeiroNumero('');
+    setSegundoNumero('');
+    setOperador(null);
+    setResultado(null);
+    setControleNumeral(1);
   }
 
-  function handleIgual(){
-    switch(operador){
+  function handleIgual() {
+    switch (operador) {
       case '+':
-          setResultado(Number(primeiroNumero) + Number(segundoNumero));
-          break;
+        setResultado(Number(primeiroNumero) + Number(segundoNumero));
+        break;
       case '-':
-          setResultado(Number(primeiroNumero) - Number(segundoNumero));
-          break;
+        setResultado(Number(primeiroNumero) - Number(segundoNumero));
+        break;
       case '/':
-          setResultado(Number(primeiroNumero) / Number(segundoNumero));
-          break;
+        setResultado(Number(primeiroNumero) / Number(segundoNumero));
+        break;
+      case '*':
+        setResultado(Number(primeiroNumero) * Number(segundoNumero));
+        break;
+      default:
+        setResultado('Erro');
     }
-    handleClear()
+    handleClear();
   }
 
   return (
     <View style={styles.container}>
-      
+
       <View style={styles.telaDeResultado}>
-        <Text style={{fontSize:80}}>{primeiroNumero}</Text>
-        <Text style={{fontSize:80}}>{operador}</Text>
-        <Text style={{fontSize:80}}>{segundoNumero}</Text>
+        <Text style={{ fontSize: 80 }}>{primeiroNumero}</Text>
+        <Text style={{ fontSize: 80 }}>{operador}</Text>
+        <Text style={{ fontSize: 80 }}>{segundoNumero}</Text>
       </View>
 
       <View style={styles.containerNumeral}>
-        <TouchableOpacity style={styles.botaoNumeral} onPress={()=> handleNumeral(1)}>
-          <Text style={{fontSize:80}}>1</Text>
+        {[1, 2, 3].map((num) => (
+          <TouchableOpacity key={num} style={styles.botaoNumeral} onPress={() => handleNumeral(num)}>
+            <Text style={{ fontSize: 80 }}>{num}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+
+      <View style={styles.containerNumeral}>
+        {[4, 5, 6].map((num) => (
+          <TouchableOpacity key={num} style={styles.botaoNumeral} onPress={() => handleNumeral(num)}>
+            <Text style={{ fontSize: 80 }}>{num}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+
+      <View style={styles.containerNumeral}>
+        {[7, 8, 9].map((num) => (
+          <TouchableOpacity key={num} style={styles.botaoNumeral} onPress={() => handleNumeral(num)}>
+            <Text style={{ fontSize: 80 }}>{num}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+
+      <View style={styles.containerNumeral}>
+        <TouchableOpacity style={styles.botaoNumeral} onPress={() => handleNumeral(0)}>
+          <Text style={{ fontSize: 80 }}>0</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.botaoNumeral} onPress={()=> handleNumeral(2)}>
-          <Text style={{fontSize:80}}>2</Text>
+        <TouchableOpacity style={styles.botaoNumeral} onPress={() => handleOperacao('*')}>
+          <Text style={{ fontSize: 80 }}>*</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.botaoNumeral} onPress={()=> handleNumeral(3)}>
-          <Text style={{fontSize:80}}>3</Text>
+        <TouchableOpacity style={styles.botaoNumeral} onPress={handleClear}>
+          <Text style={{ fontSize: 80 }}>C</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.containerNumeral}>
-        <TouchableOpacity style={styles.botaoNumeral}  onPress={()=> handleOperacao('+')}>
-          <Text style={{fontSize:80}}>+</Text>
+        <TouchableOpacity style={styles.botaoNumeral} onPress={() => handleOperacao('+')}>
+          <Text style={{ fontSize: 80 }}>+</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.botaoNumeral} onPress={()=> handleOperacao('-')}>
-          <Text style={{fontSize:80}}>-</Text>
+        <TouchableOpacity style={styles.botaoNumeral} onPress={() => handleOperacao('-')}>
+          <Text style={{ fontSize: 80 }}>-</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.botaoNumeral} onPress={()=> handleOperacao('/')}>
-          <Text style={{fontSize:80}}>/</Text>
+        <TouchableOpacity style={styles.botaoNumeral} onPress={() => handleOperacao('/')}>
+          <Text style={{ fontSize: 80 }}>/</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.containerNumeral}>
         <TouchableOpacity style={styles.botaoNumeral} onPress={handleIgual}>
-          <Text style={{fontSize:80}}>=</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botaoNumeral} onPress={handleClear}>
-          <Text style={{fontSize:80}}>C</Text>
+          <Text style={{ fontSize: 80 }}>=</Text>
         </TouchableOpacity>
       </View>
 
-      <Text style={{fontSize:80}}>
+      <Text style={{ fontSize: 80 }}>
         {resultado}
       </Text>
-
 
     </View>
   );
